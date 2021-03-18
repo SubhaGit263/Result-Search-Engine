bad68c356308cf8e9197bf74fc168ff8d7fae074
Subhankar Pandey <subhankarpandey91@gmail.com>
2021-03-19 00:05:32 +0530

    db change

--- userregistration.php
+++ userregistration.php
@@ -7,7 +7,7 @@ $db_name="db1";
 $conn=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 if(!$conn)
 {
-die("Connection Failed");
+die("Connection Failed to database");
 }
 ?>
 <!-----------------End php code for database connection------------------>
